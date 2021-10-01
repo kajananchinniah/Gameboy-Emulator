c2b370273f7abd36e4086eef1dd1822907a185f4
kajananchinniah <kajananchinniah@gmail.com>
2021-10-01 02:11:51 -0400

    Minor modification to ppu

--- src/PPU/PPU.cpp
+++ src/PPU/PPU.cpp
@@ -23,7 +23,7 @@ uint8_t PPU::get2BPPPixel(uint8_t byte1, uint8_t byte2, int position) {
 uint16_t PPU::get2BPPPixelRow(uint8_t byte1, uint8_t byte2) {
   uint16_t rval = 0x0000;
   for (int i = 0; i < 8; ++i) {
-    rval = rval | (get2BPPpixel(byte1, byte2, i) << 2 * i);
+    rval = rval | (get2BPPPixel(byte1, byte2, i) << (2 * i));
   }
   return rval;
 }
