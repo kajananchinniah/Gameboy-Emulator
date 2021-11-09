b9e952d9d0641091bf4539a1933b0a69932a3838
kajananchinniah <kajananchinniah@gmail.com>
2021-11-09 01:57:09 -0500

    Fixed minor issues with MBC1

--- src/MMU/MMU_MBC1.cpp
+++ src/MMU/MMU_MBC1.cpp
@@ -11,22 +11,20 @@ void MMU::handleBankedROMWrite_MBC1(uint16_t address, uint8_t data) {
       ram_enabled = false;
     }
   } else if (address >= 0x2000 && address <= 0x3FFF) {
-    current_rom_bank = (current_rom_bank & 0xE0) | (data & 0x1F);
+    current_rom_bank = data & 0x1F;
     if (current_rom_bank == 0x00 || current_rom_bank == 0x20 ||
         current_rom_bank == 0x40 || current_rom_bank == 0x60) {
       current_rom_bank++;
     }
   } else if (address >= 0x4000 && address <= 0x5FFF) {
-    // See:
-    // https://retrocomputing.stackexchange.com/questions/11732/how-does-the-gameboys-memory-bank-switching-work
     if (banking_mode == 0x00) {
-      current_rom_bank = (current_rom_bank & 0x1F) | ((data & 0xE0) << 5);
+      current_rom_bank = ((data & 0x03) << 5) | (current_rom_bank & 0x1F);
       if (current_rom_bank == 0x00 || current_rom_bank == 0x20 ||
           current_rom_bank == 0x40 || current_rom_bank == 0x60) {
         current_rom_bank++;
       }
     } else {
-      current_ram_bank = data & 0x3;
+      current_ram_bank = data & 0x03;
     }
 
   } else if (address >= 0x6000 && address <= 0x7FFF) {
